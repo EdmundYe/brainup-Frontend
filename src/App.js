0c89c57ca11f0fb4225ef4f4cdadd7f7ce8f20ad
EdmundYe <edmundye@cmail.carleton.ca>
2024-09-29 03:44:01 -0400

    components added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import shortlogo from "./shortlogo.png";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Form from "./form";
+import Results from "./results";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <ul className="top-header">
+          <li>
+            <img src={shortlogo} className="Header-logo" alt="logo" />
+          </li>
+          <li className="slogan">
+            <h1>Lets Fix Brainrot</h1>
+          </li>
+        </ul>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+        </header>
+
+        <Routes>
+          <Route path="/" element={<Form />} />
+          <Route path="/results" element={<Results />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
